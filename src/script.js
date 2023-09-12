7f75bc656461a1d96bc9348084a2c563581e9876
k-galvin <110426866+k-galvin@users.noreply.github.com>
2023-09-11 19:19:24 -0700

    Format code and add profit output

--- src/script.js
+++ src/script.js
@@ -1,35 +1,49 @@
-// Assigns slider and slider value to variables
-let slider = document.getElementById("time");
-let sliderOutput = document.getElementById("slider-value");
+// Variables to keep track of page elements
+const slider = document.getElementById("time");
+const sliderOutput = document.getElementById("slider-value");
+const principalInput = document.getElementById("amount");
+const rateInput = document.getElementById("rate");
+const frequencyInput = document.getElementById("frequency");
+const timeInput = document.getElementById("time");
+const totalOutput = document.getElementById("total");
+const profitOutput = document.getElementById("profit");
+const button = document.getElementById("button");
+
+// Calculates the interest given the user inputs and displays on page
+button.addEventListener('click', () => {
+  // Creates variables to store input values
+  const principal = principalInput.value;;
+  const rate = rateInput.value;
+  const frequency = frequencyInput.value;;
+  const time = timeInput.value;
+
+  // Calculates the interest
+  const total = principal * ((1 + ((rate / 100) / frequency)) ** (frequency * time));
+
+  // Calculates the profit
+  const profit = total - principal;
+
+  // Checks if total is a valid numerical input
+  if (isNaN(total)) {
+    // Displays error messages
+    totalOutput.textContent = 'Invalid input!';
+    profitOutput.textContent = 'Invalid input!';
+  } else {
+    // Displays calculated total and profit
+    totalOutput.textContent = '$' + total.toFixed(2);
+    profitOutput.textContent = '$' + profit.toFixed(2);
+  }
+
+})
 
 // Initializes the displayed slider value
 sliderOutput.innerHTML = slider.value;
 
 // Sets the slider value to update when slider is moved
-slider.oninput = function() {
-  sliderOutput.innerHTML = this.value;
-};
+slider.oninput = () => sliderOutput.textContent = slider.value;
 
-// Calculates the interest given the user inputs and displays on page
-function calculateInterest() {
-  // Assigns input values to variables
-  let originalSum = document.getElementById("amount").value;;
-  let rate = document.getElementById("rate").value;
-  let frequency = document.getElementById("frequency").value;;
-  let time = document.getElementById("time").value;
 
-  // Calculates the interest
-  let interest = originalSum * ((1 + ((rate / 100) / frequency)) ** frequency * time);
 
-  // Rounds interest value to 2 decimal points and inserts a dollar sign
-  interest = "$" + interest.toFixed(2);
-
-  // Displays calculated interest on the page
-  document.getElementById("interest").innerHTML = interest;
-
-  // Prevents form from refreshing page on button click
-  event.preventDefault();
-}
 
 
 
